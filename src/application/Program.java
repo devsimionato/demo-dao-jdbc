f18ef053c927e73c6a3ba88cef00aa78ea5e29f8
Alessandro Simionato <alessandrosimionatodev@gmail.com>
2023-08-30 14:42:29 -0300

    findById implementação

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,14 +11,13 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "books");
-		System.out.println(obj);
-
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-		
-		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
-		System.out.println(seller);
+	SellerDao sellerDao = DaoFactory.createSellerDao();
+	//instanciou um SellerDaoJDBC
+	
+	Seller seller = sellerDao.findById(3);
+	
+	System.out.println(seller);
+	
 	}
 
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,12 +1,25 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao {
 
+	private Connection conn;
+	
+	public SellerDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
+	
 	@Override
 	public void insert(Seller obj) {
 		// TODO Auto-generated method stub
@@ -27,8 +40,42 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public Seller findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*, department.Name as DepName " //selececione todos elementos de seller e department apenas nome recebendo DepName
+					+ "FROM seller INNER JOIN department" //cruze os dados das duas tabelas
+					+ " ON seller.DepartmentId = department.id " //aonde o ID de seller for igual o ID department
+					+ "WHERE seller.Id = ?"); //deste Id vendedor
+			st.setInt(1, id);
+			rs = st.executeQuery(); //a consulta preparada é executada no banco de dados usando o método executeQuery().
+			//O resultado da consulta é armazenado em um objeto ResultSet chamado rs
+			
+			//Agora precisamos transformar o rs (formato tabela) para formato objeto
+			if (rs.next()) {
+				Department dep = new Department();
+				dep.setId(rs.getInt("DepartmentId"));
+				dep.setName(rs.getString("DepName"));
+				Seller obj = new Seller();
+				obj.setId(rs.getInt("Id"));
+				obj.setName(rs.getString("Name"));
+				obj.setEmail(rs.getString("Email"));
+				obj.setBaseSalary(rs.getDouble("BaseSalary"));
+				obj.setBirthDate(rs.getDate("BirthDate"));
+				obj.setDepartment(dep);
+				return obj;
+			}
+			return null;
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+
 	}
 
 	@Override
