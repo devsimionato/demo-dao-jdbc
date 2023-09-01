90bb7844094b1a7f9ed360db5323bae1df0a4ae1
Alessandro Simionato <alessandrosimionatodev@gmail.com>
2023-09-01 14:33:17 -0300

    Incrementando findAll

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,7 +18,7 @@ public class Program {
 	System.out.println("=== TEST 1: seller findById ===");
 	Seller seller = sellerDao.findById(3);
 	System.out.println(seller);
-	*/	
+		
 	
 	System.out.println("\n=== TEST 2: seller findByDepartment ===");
 	Department department = new Department(2, null);
@@ -26,6 +26,14 @@ public class Program {
 	for (Seller obj : list) {
 		System.out.println(obj);
 	}
+	*/
+	
+	System.out.println("\n=== TEST 3: seller findAll ===");
+	List<Seller> list = sellerDao.findAll();
+	for (Seller obj : list) {
+		System.out.println(obj);
+	}
+	
 	}
 
 }
