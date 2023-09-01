f56b17928411b6dd3852700224eab1108be27740
Alessandro Simionato <alessandrosimionatodev@gmail.com>
2023-09-01 15:24:00 -0300

    Incrementando update

--- src/application/Program.java
+++ src/application/Program.java
@@ -33,13 +33,23 @@ public class Program {
 	for (Seller obj : list) {
 		System.out.println(obj);
 	}
-	*/
+
 	
 	Department department = new Department(2, null);
 	System.out.println("\n=== TEST 4: seller insert ===");
 	Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 	sellerDao.insert(newSeller);
 	System.out.println("Inserted! New id = " + newSeller.getId());
+	*/
+	
+	System.out.println("\n=== TEST 3: seller update ===");
+	Seller seller = sellerDao.findById(1);
+	seller.setName("Martha Waine");
+	//atualizando o valor da variável na classe
+	sellerDao.update(seller);
+	//atualizando no banco de dados
+	System.out.println("Update completed!");
+	
 	}
 
 }
